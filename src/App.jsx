4fe794c895feb543ba8c2db32dd4b85b4748fc83
Sakshi Santosh Adate <sakshiadate3@gmail.com>
2025-06-23 18:48:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import AdmissionsPage from './pages/AdmissionsPage';
+import ContactPage from './pages/ContactPage';
+import CoursesPage from './pages/CoursesPage';
+
+import NotFoundPage from './pages/NotFoundPage';
+
+//import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+
+function App() {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return (
+    <> 
+    
+
+    <Router>
+     
+      <main style={{ padding: '1rem' }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/apply" element={<AdmissionsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+           <Route path="/courses" element={<CoursesPage/>} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    
+    </Router>
+    </>
+  );
+}
+
+export default App;
